9900eb85eed242e2ba2f76be8bedfc7b459229ab
adarsh40parihar <22je0040@iitism.ac.in>
2025-01-07 03:02:12 +0530

    shifting from routing app to app.jsx and changes in login page.

--- Project-ChatApp/src/Components/Login.jsx
+++ Project-ChatApp/src/Components/Login.jsx
@@ -2,18 +2,32 @@
 import React from 'react'
 import whatsapplogo from "../Assets/whatsapp.svg"
 import fingerprintlogo from "../Assets/fingerprint.svg";
+import { Fingerprint, LogIn as LoginIcon } from "lucide-react";
 function Login() {
   return (
     <>
-      <div>
-        <img src={whatsapplogo} alt="WhatsApplogo" />
-        <div>WHATSAPP</div>
+      <div className="h-[220px] bg-[#15b491] ">
+        <div className="flex ml-[200px] pt-[40px] items-center gap-4">
+          <img src={whatsapplogo} alt="WhatsApplogo" className="h-8" />
+          <div className="text-white font-semibold">WHATSAPP</div>
+        </div>
       </div>
-      <div>
-        <img src={fingerprintlogo} alt="fingerprint Svg" className="h-8"/>
-        <div>Sign In</div>
-        <div>Sign In with Your Google account to get started.</div>
-        <button>Sign in with Google</button>
+
+      <div className="h-[calc(100vh-220px)] bg-[#eff2f5] flex items-center justify-center relative">
+        <div className="bg-white h-[90%] w-[70%] shadow-2xl flex flex-col items-center justify-center gap-4 absolute -top-[93px] rounded">
+          <Fingerprint
+            className="h-20 w-20 text-primary"
+            strokeWidth={1.1}
+            style={{ color: "#15b491" }}
+          />
+          <div className="text-xl font-medium">Sign In</div>
+          <div className="text-[#606060]">
+            Sign In with Your Google account to get started.
+          </div>
+          <button className="bg-[#15b491] text-white p-4  rounded hover:border-blue-500">
+            Sign in with Google
+          </button>
+        </div>
       </div>
     </>
   );
